a78419c70213cd91125bcfff79551b4f0a728383
Samuel Kigbu <kigbusamul5@gmail.com>
2025-09-14 02:18:20 +0100

    added request.php and session.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+
+use App\Core\Session;
+
 require 'vendor/autoload.php'; 
+
+$session = new Session;
+$session->set('name', 'John Doe');
+echo $session->get('name');
 
 echo'<prev>';
 var_dump(App\Core\Request::uri());
